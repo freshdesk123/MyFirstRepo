80ca28cea776fa586a621ac15d5afbcd4a542f40
VinothChinnadurai <vinoth.c@freshdesk.com>
2018-09-04 20:59:45 +0530

    tests

---
+++ test/test1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class OutputFileTest		
+{
+    private File output; 
+    @Before public void createOutputFile() 
+    { 
+       output = new File(...);
+    }
+  
+	@After public void deleteOutputFile() 
+    {
+        output.delete(); 
+    } 
+     
+    @Test public void testFile1() 
+    {
+       // code for test case objective
+    } 
+	@Test public void testFile2() 
+    {
+       // code for test case objective
+    }
+}
+
